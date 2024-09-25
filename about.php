d0828cb1d73b59b743a51b348d985de6f9a0b646
tianglstrik <tiannuah@gmail.com>
2024-09-25 10:32:29 +0700

    tentang ku

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact me</title>
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="about.php">Profil</a></li>
+            <li class="dropdown"><a href="#">Materi</a>
+              <ul>
+                <li><a href="penulisan.docx">Penulisan css</a></li>
+                <li><a href="#">PHP</a></li>
+                <li><a href="#">Jquery</a></li>
+                <li><a href="#">HTML</a>
+                  <ul>
+                    <li><a href="#">Dasar HTML</a></li>
+                    <li><a href="#">Tag HTML</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li><a href="contact.php">Kontak</a></li>
+            <li><a href="register.php">Register</a></li>
+        </ul>
+    </nav>
+    
+    <hr>
+
+    <article>
+        <h1>About me</h1>
+        <p>
+            Hi tuan bagaimana kabar mu, kali ini saya akan menjelaskan data budak yang akan saya berikan tuan.
+        </p>
+        <p>
+            Nama budak adalah memet yang bisa mengurus rumah tangga dan membantu mu dalam melakukan hal yang kau ingin kan.
+        </p>
+        <p>
+            Budak ini juga punya bakat dalam perkodingan ya ya ya saya setuju jangan lupa subrek hewahunja.
+        </p>
+
+        <i frame="">
+    </article>
+
+    <hr>
+    <footer style="text-align:center;">
+        <p>Copyright &copy; 2024 Ahmad Cristiano</p>
+    </footer>
+</body>
+</html>
