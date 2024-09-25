5c00fbd910bd1d58d80663b4cbbdf0f1c12991f9
tianglstrik <tiannuah@gmail.com>
2024-09-25 08:58:11 +0700

    index ku

--- index.php
+++ index.php
@@ -3,9 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Ahmad Cristiano Nuah Personal Website</title>
 </head>
+<link rel="stylesheet" href="style.css">
 <body>
-    <h1>selamat datang</h1>
+  <nav>
+      <ul>
+          <li><a href="index.php">Beranda</a></li>
+          <li><a href="about.php">Profil</a></li>
+          <li class="dropdown"><a href="#">Materi</a>
+            <ul>
+              <li><a href="penulisan.docx">Penulisan css</a></li>
+              <li><a href="#">PHP</a></li>
+              <li><a href="#">Jquery</a></li>
+              <li><a href="#">HTML</a>
+                <ul>
+                  <li><a href="#">Dasar HTML</a></li>
+                  <li><a href="#">Tag HTML</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li><a href="contact.php">Kontak</a></li>
+          <li><a href="register.php">Register</a></li>
+      </ul>
+  </nav>
+  
+    <hr>
+    <header style="text-align:center;">
+        <img src="img/memet.jpg" width="200" height="200" style="border-radius:50%;">
+        <h1>Ahmad Cristiano Nuah</h1>
+        <p>(Web Developer Pemula)</p>
+    </header>
+    <hr>
+
+    <article style="text-align:center;">
+        <h2>Overview</h2>
+        <p>
+            Hi, tuan selamat siang jika anda mencari budak kulit hitam bisa menghubungi saya dengan melalui web ini.
+            Saat ini detik saya terngiang dj kamen rider.
+        </p>
+    </article>
+
+    <div style="max-width:600px; margin:3em auto;">
+        <table border="1" width="100%">
+            <thead>
+                <tr>
+                    <th>Skill</th>
+                    <th>Pengalaman</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li>HTML(pemula)</li>
+                            <li>CSS (amatir)</li>
+                            <li>JavaScript (IM STRONGER)</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <li>magang di tempat leadmarket</li>
+                            <li>pernah glory</li>
+                            <li>pernah savage pake ling</li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <hr>
+
+    <footer style="text-align:center;">
+        <p>Copyright &copy; 2024 ahmad cristiano</p>
+    </footer>
 </body>
 </html>
